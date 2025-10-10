10de43c5d5b2d71f5f3905670d0e171cfda3ce1b
simplekile <duonganh.mmc@gmail.com>
2025-10-10 08:16:41 +0700

    fix: use findChild(QMenuBar) to access menu bar - main window is QWidget not QMainWindow

--- python/mono_tools/file_manager/file_manager_menu_integration.py
+++ python/mono_tools/file_manager/file_manager_menu_integration.py
@@ -13,12 +13,14 @@ _minibar_instance = None
 def add_file_manager_to_menu():
     """Thêm File Manager vào menu Houdini"""
     try:
-        # Get main window and menu bar using Qt
+        # Get main window
         main_window = hou.qt.mainWindow()
         if not main_window:
             return False
         
-        menu_bar = main_window.menuBar()
+        # Find menu bar - it's a child of main window
+        from mono_tools.qt import QtWidgets
+        menu_bar = main_window.findChild(QtWidgets.QMenuBar)
         if not menu_bar:
             return False
         
