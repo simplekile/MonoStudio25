81441c86ef501610b4cd4beba0a03a66e908fec7
simplekile <duonganh.mmc@gmail.com>
2025-10-31 08:36:43 +0700

    chore: remove internal tools from git tracking
    - Remove migration/ scripts (one-time migration, not needed for users)
    - Remove analysis/ scripts (internal structure analysis)
    - Remove testing/ scripts (internal development tools)
    - These are already in .gitignore but were tracked before
    - Keep files locally for development but don't include in repository

--- python/analysis/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Analysis Tools Package
-Tools for analyzing project structure and organization
-"""
-
-# Analysis tools are utility scripts, not imported modules
-# They are meant to be run directly for analysis purposes
-
-__all__ = []
